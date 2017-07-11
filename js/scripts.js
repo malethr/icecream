4ac34306eeab27c481f51264ccda45d3eaa792fc
Maria Thomas <malethr@gmail.com>
2017-07-11 14:54:03 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
       $("ul").append("<li>"+flavor );
     });
 });
+
+// icecream
+$(document).ready(function(){
+
+  $("#factorial").submit(function(event){
+    event.preventDefault();
+    var response = ($("#word").val()).toLowerCase();
+    var forward = response.split('');
+    var forward1 = forward.join('');
+    var backwards = forward.reverse();
+    var backwards1 = backwards.join('');
+
+    if (backwards1===forward1) {
+      return alert("It's a palindrome.")
+    } else {
+      return alert("It's not a palindrome.")
+
+    }
